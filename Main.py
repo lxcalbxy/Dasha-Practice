f2c36a8b5a95a931f3258e6fa5c68e9b95ba8509
localboyprod <71847945+lxcalbxy@users.noreply.github.com>
2024-02-13 13:43:25 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+import os
+os.system('cls')
+# Чтобы этот код работал нужно начинать писать следующую логику в функции "MainLogic()"
+
+def MainLogic():
+  print()
+  # Код здесь...
+
+if __name__ == "__main__":
+  MainLogic()
